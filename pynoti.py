a85e84f68a29a1a8ca637a957c9973e0d6025ad3
Daniel Azamar <dazamar01@hotmail.com>
2023-08-14 09:15:09 -0600

    Add files via upload

---
+++ pynoti.py
@@ -0,0 +1,18 @@
+# pip3 install pypdf2
+import os
+from PyPDF2 import PdfReader, PdfWriter
+
+path = "/Users/danielazamar-chacha/Desktop/notificacion/notificacion.pdf"
+fname = "noti_"
+
+pdf = PdfReader(path)
+for page in range(len(pdf.pages)):
+    pdf_writer = PdfWriter()
+    pdf_writer.add_page(pdf.pages[page])
+
+    output_filename = '{}_page_{}.pdf'.format(fname, page+1)
+
+    with open(output_filename, 'wb') as out:
+        pdf_writer.write(out)
+
+    print('Created: {}'.format(output_filename))
